0355686d2b63696c8f1fa60e7492d346b4cca3d8
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-05-29 21:44:17 -0300

    Em desenvolvimento, add metodo adicionar participante

--- times/views.py
+++ times/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from times.serializers import TimeSerializer
 from rest_framework.permissions import IsAuthenticated
 from times.models import Time
+from times.permissions import IsGerenteOrReadOnly
 
 class TimeViewSet(viewsets.ModelViewSet):
     serializer_class = TimeSerializer
@@ -9,3 +10,7 @@ class TimeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Time.objects.filter(participantes__usuario=self.request.user)
+    
+    @action(methods=['post'], detail=True, permission_classes=[IsGerenteOrReadOnly])
+    def adicionar_participante(self, request, pk=None):
+        time = Time.objects.get(pk=pk)
